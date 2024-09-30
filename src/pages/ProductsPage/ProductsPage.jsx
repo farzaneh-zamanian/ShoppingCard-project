77037092f372347031b6d09553d2bed918cb9fd7
Farzaneh Zamanian <152534852+farzaneh-zamanian@users.noreply.github.com>
2024-09-30 17:44:29 +0330

    Loading added

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,13 +1,14 @@
 import styles from "./ProductsPage.module.css";
 import { useProducts } from "../../context/ProductContext";
 import Card from "../../components/Card/Card";
+import Loading from "../../components/Loading/Loading";
 function ProductsPage() {
   const { products } = useProducts();
 
   return (
     <div className={styles.container}>
       <div className={styles.productsContainer}>
-        {!products.length && <p>...Loading</p>}
+        {!products.length && <Loading/>}
         {products.map((product) => (
           <Card key={product.id} product={product} />
         ))}
